25eae714d0297f190a7efd0e530ecaed8321dc33
GordonLucasSyn <113020395+GordonLucasSyn@users.noreply.github.com>
2024-02-07 13:09:11 +0100

    Tag 3: Bonusaufgabe

---
+++ Woche-1/Tag-3/src/Bonusaufgabe.kt
@@ -0,0 +1,23 @@
+fun main(){
+    /*
+        Gegeben sind die durchschnittlichen Distanzen der Planeten unseres Sonnensystems zur Sonne (in AU).
+
+        Eine AU (= Astronomische Einheit) ist die Distanz von der Erde zur Sonne.
+            (also ca. 149597870 km)
+
+        Merkur: 0,39 AE
+        Venus: 0,72 AE
+        Mars: 1,52 AE
+        Jupiter: 5,20 AE
+        Saturn: 9,58 AE
+        Uranus: 19,22 AE
+        Neptun: 30,05 AE
+
+        TODO: Für alle von diesen Planeten rechne nun die Distanzen um in KM und drucke sie auf der Konsole aus.
+
+        Tipp: Manche Distanzen sind zu groß um sie in einem Integer zu speichern ;)
+        https://kotlinlang.org/docs/numbers.html
+     */
+
+
+}
