cbfb42b2b46b42c6a2f37d6722761858c756484f
GordonLucasSyn <113020395+GordonLucasSyn@users.noreply.github.com>
2024-02-08 09:55:43 +0100

    Tag 4: Packages

--- Woche-1/Tag-4/heutigerCode/BankEinzahlung.kt
+++ Woche-1/Tag-4/heutigerCode/BankEinzahlung.kt
@@ -1,22 +1,24 @@
+import banking.*
 
 fun main(){
-
     var kontostandInYen: Double = 45123.23
-    val wechselkurs: Int = 142      // 1 euro = 130 Yen
+
 //    val wechselkurs: Double = 142.0      // 1 euro = 130 Yen
 
     // Was ist der Kontostand in Euro?
-    var kontostandInEuro: Double = kontostandInYen / wechselkurs
+    var kontostandInEuro: Double = kontostandInYen / wechselKursEuroToYen
     println("Kontostand in Euro: $kontostandInEuro")
 
     // Einzahlung:
     print("Einzahlung: ")
-    var einzahlung: Double = readln().toDouble()
+    val einzahlung: Double = readln().toDouble()
 
-    kontostandInEuro += einzahlung
-    kontostandInYen += einzahlung * wechselkurs
+    kontostandInEuro = kontostandInEuro + einzahlung
+    kontostandInYen += einzahlung * wechselKursEuroToYen
 
     println("Ihr neuer Kontostand: $kontostandInEuro €")
     println("Ihr neuer Kontostand: $kontostandInYen ¥")
 
 }
+
+
---
+++ Woche-1/Tag-4/heutigerCode/banking/wechselkurse.kt
@@ -0,0 +1,4 @@
+package banking     // alles in dieser Datei gehört zum Package banking
+
+val wechselKursEuroToYen: Double = 142.0
+val wechselKursEuroToDollar: Double = 0.9
