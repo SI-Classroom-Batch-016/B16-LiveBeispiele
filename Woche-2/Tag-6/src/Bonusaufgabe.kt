6f38c01c35f85474feb184a80918492976def1e8
GordonLucasSyn <113020395+GordonLucasSyn@users.noreply.github.com>
2024-02-12 13:12:35 +0100

    Tag 6: Listen und Indices Bonusaufgabe

---
+++ Woche-2/Tag-6/src/Bonusaufgabe.kt
@@ -0,0 +1,24 @@
+
+fun main(){
+
+    /*
+        Gegeben ist eine Liste aus Produkten, inkl. Artikel Name und Preis
+
+        TODO:
+            Deine Aufgabe ist es einem Nutzer eine Preisabfrage der Artikel via der Konsole zu ermöglichen
+     */
+
+    var automat: List<Pair<String, Double>> = listOf(
+        "Twix" to 2.00,
+        "Mars" to 2.00,
+        "Chio Chips" to 2.50,
+        "Lays Chips" to 2.50,
+        "Coca Cola 0.33" to 3.00,
+        "Fanta 0.33" to 3.00,
+        "Sprite 0.33" to 3.00,
+    )
+
+
+
+
+}
