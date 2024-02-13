ee19cdfe84aa237fdbb2b42094dce99d0b0ddb0a
GordonLucasSyn <113020395+GordonLucasSyn@users.noreply.github.com>
2024-02-13 12:42:24 +0100

    Tag 7: Listenfunktionen Bonusaufgabe

---
+++ Woche-2/Tag-7/src/Bonusaufgabe:Top3.kt
@@ -0,0 +1,51 @@
+fun main(){
+
+    /*
+        TODO:
+         Gegeben sind eine Liste von Spielern und Punktzahlen
+         Die Indices der beiden Listen stehen für die selbe SpielerIn
+         Finde die 3 Spielerinnen mit den höchsten Punktzahlen,
+         und geb sie auf der Konsole aus. (sowohl Name als auch Punkte)
+     */
+
+    val players = mutableListOf<String>(
+        "Nadia",
+        "Anna",
+        "Christian",
+        "Marco",
+        "Davide",
+        "Killian",
+        "Theresa",
+        "Augusto",
+        "Julian",
+        "Maria"
+    )
+
+    val points = mutableListOf<Int>(
+        (50..100).random(),
+        (50..100).random(),
+        (50..100).random(),
+        (50..100).random(),
+        (50..100).random(),
+        (50..100).random(),
+        (50..100).random(),
+        (50..100).random(),
+        (50..100).random(),
+        (50..100).random(),
+    )
+    
+    println("""
+        ---------------------------
+        ${players[0]}: ${points[0]}
+        ${players[1]}: ${points[1]}
+        ${players[2]}: ${points[2]}
+        ${players[3]}: ${points[3]}
+        ${players[4]}: ${points[4]}
+        ${players[5]}: ${points[5]}
+        ${players[6]}: ${points[6]}
+        ${players[7]}: ${points[7]}
+        ${players[8]}: ${points[8]}
+        ${players[9]}: ${points[9]}
+        ---------------------------
+    """.trimIndent())
+}
