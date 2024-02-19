ac43797323cd7cca1150b656c7df7c8edfdee101
GordonLucasSyn <113020395+GordonLucasSyn@users.noreply.github.com>
2024-02-19 13:04:08 +0100

    Tag 11: Funktionen und kotlin.Unit

---
+++ Woche-3/Tag-11/src/BonusAutomat.kt
@@ -0,0 +1,45 @@
+val automat: MutableMap<String, Double> = mutableMapOf(
+    "Snickers" to 1.50,
+    "Mars" to 1.50,
+    "Twix" to 1.50,
+    "Milky Way" to 1.00,
+    "Kitkat" to 1.00,
+    "Haribo Goldbären" to 2.00,
+    "Haribo Balla Balla" to 2.00
+)
+
+var guthaben: Double = 10.0
+
+fun main(){
+    /* TODO:
+        Rufe hier die anderen 3 Funktionen in der richtigen Reihenfolge auf.
+           Schaffst du es auch, dass der Automat nicht nach einer Auswahl beendet ist?
+     */
+}
+
+fun angebotAnzeigen(){
+    /* TODO:
+        Diese Funktion soll mit Hilfe der Println() eine saubere Übersicht bieten,
+            über alle zur Auswahl stehenden Produkte und Preise.
+        Idealerweise, beschreibt diese Ausgabe auch, welche Eingabe für welches Produkt zu treffen ist.
+     */
+
+}
+
+
+
+fun auswahlTreffen(){
+    /* TODO:
+        Diese Funktion soll dem Kunden erlauben, via der Konsole ein Produkt aus dem Automaten zu wählen,
+            zB via einer Zahleneingabe.
+
+       TIP: automat.keys[index]
+     */
+}
+
+
+fun rueckgeldBerechnen(){
+    /* TODO:
+        Diese Funktion soll das verbleibende Guthaben im Automaten berechnen
+     */
+}
