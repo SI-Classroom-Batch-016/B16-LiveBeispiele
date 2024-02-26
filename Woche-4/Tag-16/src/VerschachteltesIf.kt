e288bc4ad4824ea463559fffd2a6c60945bd0b0b
GordonLucasSyn <113020395+GordonLucasSyn@users.noreply.github.com>
2024-02-26 13:29:28 +0100

    Tag 16: Verzweigungen: Aufgabe 7

--- Woche-4/Tag-16/src/VerschachteltesIf.kt
+++ Woche-4/Tag-16/src/VerschachteltesIf.kt
@@ -1,16 +1,24 @@
 
+val YES_OPTIONS = listOf(
+    "ja", "yes", "j", "y", "JA", "ok", "OK","👍🏻"
+)
 
 fun main(){
 
     // 2 Eingaben:
-    println("Regnet es?")
-    var regnetEs: Boolean = readln().contains("ja")
+//    println("Regnet es?")
+//    var regnetEs: Boolean = readln().contains("ja")
+
+    var regnetEs = readBoolean("Regnet es?")
+//    var rausgehen = readBoolean("Gehst du raus?")
 
     println("Gehst du heute raus?")
 //    var rausgehen: Boolean = readln().contains("ja")
 
     var rausgehen: Boolean  = when (readln()){
         "ja", "yes", "j", "y", "JA", "ok", "OK","👍🏻" -> true
+        in YES_OPTIONS -> true
+        !in YES_OPTIONS -> false
         else -> false
     }
 
@@ -31,4 +39,15 @@ fun main(){
     }
 //    Wenn der Nutzer nicht rausgeht und es regnet, soll ihm empfohlen werden die Fenster zu schließen
 //    Wenn der Nutzer nicht rausgeht und es nicht regnet, brauch ihm nichts empfohlen werden.
+}
+
+
+fun readBoolean(prompt: String): Boolean {
+    println(prompt)
+    return when (readln()){
+        "ja", "yes", "j", "y", "JA", "ok", "OK","👍🏻" -> true
+        in YES_OPTIONS -> true
+        !in YES_OPTIONS -> false
+        else -> readBoolean(prompt)
+    }
 }
